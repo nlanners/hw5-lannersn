69c6858a9e2e2a6921830b97471020d8fc3ed546
Nick Lanners <lannersn@oregonstate.edu>
2022-04-30 16:07:47 -0600

    finished

--- api/boats.js
+++ api/boats.js
@@ -9,29 +9,19 @@ const datastore = ds.datastore;
 router.use(bodyParser.json());
 
 const c = constants.constants;
-
+const m = constants.messages;
 
 
 /* ------------------------------------------------- BEGIN MODEL FUNCTIONS ---------------------------------------- */
-
-// hash name
-// source: https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
-// function hash_name (str, seed = 0) {
-//     let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-//     for (let i = 0, ch; i < str.length; i++) {
-//         ch = str.charCodeAt(i);
-//         h1 = Math.imul(h1 ^ ch, 2654435761);
-//         h2 = Math.imul(h2 ^ ch, 1597334677);
-//     }
-//     h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-//     h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-//     return 4294967296 * (2097151 & h2) + (h1>>>0);
-// };
 
 
 // POST a boat
 async function post_boat(name, type, length, url) {
     let key = datastore.key(c.BOAT);
+
+    if (!check_name(name)) {
+        return [c.BAD_REQUEST, m.BAD_REQUEST_NAME];
+    }
 
     try {
         if (await is_name_unique(name)) {
@@ -40,14 +30,14 @@ async function post_boat(name, type, length, url) {
             const [boat] = await datastore.get(key);
             boat.self = url + boat[ds.Datastore.KEY].id;
             await datastore.update({key:key, data:boat});
-            return ds.fromDatastore(boat);
+            return [c.CREATED, ds.fromDatastore(boat)];
         } else {
-           return c.FORBIDDEN;
+           return [c.FORBIDDEN, null];
         }
     } catch (err) {
         console.log('post_boat');
         console.log(err);
-        return c.ERROR;
+        return [c.ERROR, null];
     }
 
     
@@ -58,9 +48,9 @@ async function get_boat(id) {
     try {
         const key = datastore.key([c.BOAT, parseInt(id, 10)]);
         const [boat] = await datastore.get(key);
-        return ds.fromDatastore(boat);
+        return [c.OK, ds.fromDatastore(boat)];
     } catch (err) {
-        return false;
+        return [c.NOT_FOUND, null];
     }
 }
 
@@ -74,32 +64,37 @@ async function get_all_boats() {
     } catch (err) {
         console.log('get_all_boats');
         console.log(err);
+        return c.ERROR;
     }
 }
 
 // Delete a boat from database by id
 async function delete_boat(id) {
     try {
-        let boat = await get_boat(id);
+        const key = datastore.key([c.BOAT, parseInt(id, 10)]);
+    
+        const results = await datastore.delete(key);
 
-        if (!boat) {
+        if (results[0]. indexUpdates === 0){
             return c.NOT_FOUND;
+
+        } else {
+            return c.NO_CONTENT;
         }
-    
-        for (let l in boat.loads) {
-            let load = await Loads.get_load(boat.loads[l].id);
-            load.carrier = null;
-            await datastore.update(ds.createEntity(load));
-        }
-    
-        return datastore.delete(boat[ds.Datastore.KEY]);
+        
     } catch (err) {
+        console.log('delete_boat');
         console.log(err);
+        return c.ERROR;
     }   
 }
 
 // Modify all attributes of a boat
 async function put_boat(id, name, type, length, url) {
+    if (!check_name(name)) {
+        return c.BAD_REQUEST;
+    }
+
     try {
         const key = datastore.key([c.BOAT, parseInt(id, 10)]);
         const boat = {"name":name, "type":type, "length":length, "self":url};
@@ -122,13 +117,16 @@ async function put_boat(id, name, type, length, url) {
 // Patch any subset of boat attributes
 async function patch_boat(id, body) {
     let boat = await get_boat(id);
+    boat = boat[1];
 
-    if (body.name) {
+    if (body.name && check_name(body.name)) {
         if (await is_name_unique(body.name)){
             boat.name = body.name;
         } else {
-            return [c.FORBIDDEN];
+            return [c.FORBIDDEN, null];
         }
+    } else {
+        return [c.BAD_REQUEST, m.BAD_REQUEST_NAME];
     }
     boat.type = body.type || boat.type;
     boat.length = body.length || boat.length
@@ -142,6 +140,7 @@ async function patch_boat(id, body) {
     } catch (err) {
         console.log('patch_boat');
         console.log(err);
+        return [c.ERROR, null];
     }
 }
 
@@ -160,13 +159,15 @@ async function is_name_unique(name) {
         return true;
 
     } catch (err) {
+        console.log('is_name_unique');
         console.log(err);
+        return false;
     }
 }
 
 // create an HTML representation of a boat
 function createHTML(boat) {
-    let html = `<ul>`;
+    let html = '<ul>';
     const keys = Object.keys(boat);
 
     for (let key of keys) {
@@ -177,6 +178,77 @@ function createHTML(boat) {
     return html;
 }
 
+// validate boat name
+function check_name(name) {
+    if (name.length <= c.name_length) {
+        return name.match(c.is_alpha);
+    } else {
+        return false;
+    }
+}
+
+// send appropriate response
+function handle_response(res, result, extra=null) {
+    switch (result) {
+        case c.BAD_METHOD:
+            res.set('Accept', 'GET, POST');
+            res.status(c.BAD_METHOD).send(m.BAD_METHOD);
+            break;
+
+        case c.BAD_REQUEST:
+            res.status(c.BAD_REQUEST).send(extra);
+            break;
+
+        case c.FORBIDDEN:
+            res.status(c.FORBIDDEN).send(m.FORBIDDEN);
+            break;
+
+        case c.CREATED:
+            res.status(c.CREATED).location(extra.self).json( extra );
+            break;
+
+        case c.ERROR:
+            res.status(c.ERROR).send(extra);
+            break;
+
+        case c.NO_CONTENT:
+            res.status(c.NO_CONTENT).end();
+            break;
+
+        case c.NOT_ACCEPTABLE:
+            res.status(c.NOT_ACCEPTABLE).send(m.NOT_ACCEPTABLE + extra);
+            break;
+
+        case c.NOT_FOUND:
+            res.status(c.NOT_FOUND).send(m.NOT_FOUND);
+            break;
+        
+        case c.OK:
+            // send HTML
+            if (extra[1] === 'text/html'){
+                res.status(c.OK).send(createHTML(extra[0]));
+
+            // send JSON
+            } else if (extra[1] === 'application/json') {
+                res.status(c.OK).json(extra[0]);
+
+            // error
+            } else {
+                res.status(c.ERROR).send(message);
+            }
+            break;
+            
+        case c.SEE_OTHER:
+            res.status(c.SEE_OTHER).location(extra).end();
+            break;
+
+        case c.UNSUPPORTED:
+            res.status(c.UNSUPPORTED).send(m.UNSUPPORTED);
+            break;
+
+    }
+}
+
 /* --------------------------------------------- END MODEL FUNCTIONS ------------------------------------------------- */
 
 /* ---------------------------------------- BEGIN CONTROLLER FUNCTIONS ----------------------------------------------- */
@@ -185,35 +257,34 @@ function createHTML(boat) {
 router.post('/', async (req, res) => {
     const accepts = req.accepts(['application/json']);
 
+    // check request content type is correct
     if (req.get('content-type') !== 'application/json') {
-        res.status(c.UNSUPPORTED).send("POST data must be sent as Content-Type: application/json");
+        handle_response(res, c.UNSUPPORTED);
 
+    // check client response acceptable
     } else if (!accepts) {
-        res.status(c.NOT_ACCEPTABLE).send("Response must be sent as application/json. Please change Accept request header.");
+        handle_response(res, c.NOT_ACCEPTABLE, 'application/json');
 
+    // validate attributes
     } else if (req.body.name && req.body.type && req.body.length) {
 
         try {
+            // create boat in database
             const url = ds.createURL(req);
             const boat = await post_boat(req.body.name, req.body.type, req.body.length, url);
 
-            switch (boat) {
-                case c.FORBIDDEN:
-                    res.status(c.FORBIDDEN).send("Boat name has already been used. Please use a unique name.");
-                    break;
-                case c.ERROR:
-                    res.status(c.ERROR).send("Something went wrong creating the boat. Please try again");
-                    break;
-                default:
-                    res.status(c.CREATED).location(boat.self).json( boat );
-            }
+            // handle response
+            handle_response(res, boat[0], boat[1]);
+            
         } catch (err) {
+            console.log('router.post');
             console.log(err);
-            res.status(c.ERROR).send("Something went wrong creating the boat. Please try again");
+            handle_response(res, c.ERROR);
         }
 
+    // attributes failed
     } else {
-        res.status(c.BAD_REQUEST).send("The request object is missing at least one of the required attributes");
+        handle_response(res, c.BAD_REQUEST, m.BAD_REQUEST_ATTR);
     }
 })
 
@@ -221,145 +292,139 @@ router.post('/', async (req, res) => {
 // DELETE boat
 router.delete('/:boat_id', async (req, res) => {
     try {
-        const stuff = await delete_boat(req.params.boat_id)
-        if (stuff === c.NOT_FOUND) {
-            res.status(c.NOT_FOUND).send("No boat with this boat_id exists");
-        } else {
-            res.status(c.NO_CONTENT).end();
-        }
+        // remove boat from database
+        const result = await delete_boat(req.params.boat_id)
+        handle_response(res, result);
+
     } catch (err) {
+        console.log('router.delete');
         console.log(err);
+        handle_response(res, c.ERROR);
     }
 });
 
 // EDIT a boat
-// request and response must be JSON
-// updating the value of id is not allowed
-// must support PUT and PATCH
 
 // PATCH allows updating any subset of attributes while the others remain unchanged
 router.patch('/:boat_id', async (req, res) => {
     const accepts = req.accepts(['application/json']);
+
+    // check request content type
     if (req.get('content-type') !== 'application/json') {
-        res.status(c.UNSUPPORTED).send("POST data must be sent as Content-Type: application/json")
+        handle_response(res, c.UNSUPPORTED);
+
+    // check response acceptable
     } else if (!accepts) {
-        res.status(c.NOT_ACCEPTABLE).send("Response must be sent as application/json. Please change Accept request header.")
+        handle_response(res, c.NOT_ACCEPTABLE);
+
     } else {
         try {
+            // patch boat in database
             const result = await patch_boat(req.params.boat_id, req.body);
 
-            switch (result[0]) {
-                case c.SEE_OTHER:
-                    res.status(c.SEE_OTHER).location(result[1]).end();
-                    break;
-                case c.FORBIDDEN:
-                    res.status(c.FORBIDDEN).send("Boat name must be unique");
-                    break;
-                default:
-                    res.status(c.ERROR).send("Something broke. Please try again.");
-            }
+            handle_response(res, result[0], result[1]);
+            
         } catch (err) {
             console.log('router.patch');
             console.log(err);
+            handle_response(res, c.ERROR);
         }
     }
-    
-})
+});
 
 // PUT all attributes modified (except id)
-// return status 303
 router.put('/:boat_id', async (req, res) => {
+    const accepts = req.accepts(['application/json']);
+
+    // check request content type
     if (req.get('content-type') !== 'application/json') {
-        res.status(c.UNSUPPORTED).send("POST data must be sent as Content-Type: application/json")
-    } else if (req.headers.accept !== 'application/json' && req.headers.accept !== '*/*') {
-        res.status(c.NOT_ACCEPTABLE).send("Response must be sent as application/json. Please change Accept request header.")
+        handle_response(res, c.UNSUPPORTED);
+
+    // check client response acceptable
+    } else if (!accepts) {
+        handle_response(res, c.NOT_ACCEPTABLE);
+
+    // check if client trying to change id.
     } else if (req.body.id) {
-        res.status(c.BAD_REQUEST).send("Attribute 'id' can not be modified.");
+        handle_response(res, c.BAD_REQUEST, m.BAD_REQUEST_ID);
+
+    // check attributes
     } else if (req.body.name && req.body.type && req.body.length) {
         try {
+            // put boat in database
             const url = ds.createURL(req) + req.url.slice(1);
             const result = await put_boat(req.params.boat_id, req.body.name, req.body.type, req.body.length, url)
-            switch (result) {
-                case c.SEE_OTHER:
-                    res.location(url);
-                    res.status(c.SEE_OTHER).end();
-                    break;
 
-                case c.FORBIDDEN:
-                    res.status(c.FORBIDDEN).send('Boat name must be unique');
-                    break;
-
-                default:
-                    res.status(c.ERROR).send("Something didn't work correctly. Please try again.");
-                    break;
-            }
+            // handle response
+            handle_response(res, result, url);
             
         } catch (err) {
             console.log('router.put');
             console.log(err);
+            handle_response(res, c.ERROR);
         }
+    } else {
+        handle_response(res, c.BAD_REQUEST, m.BAD_REQUEST_ATTR);
     }
 })
 
 // VIEW a boat
 // JSON or HTML as determined by client's Accept header
 router.get('/:boat_id', async (req, res) => {
+
+    // check client response acceptable
     const accepts = req.accepts(['application/json', 'text/html']);
     if (!accepts) {
-        res.status(c.NOT_ACCEPTABLE).send("Response must be sent as application/json or text/html. Please change Accept request header.")
+        handle_response(res, c.NOT_ACCEPTABLE, ['application/json', 'text/html']);
+
     } else {
-        const boat = await get_boat(req.params.boat_id)
-        if (boat) {
-            if (accepts === 'text/html'){
-                res.status(c.OK).send(createHTML(boat));
-            } else if (accepts === 'application/json') {
-                res.status(c.OK).json(boat);
-            } else {
-                res.status(c.ERROR).send('Content type got messed up');
-            }
+        try {
+            // get boat
+            const result = await get_boat(req.params.boat_id)
+
+            handle_response(res, result[0], [result[1], accepts]);
             
-        } else {
-            res.status(c.NOT_FOUND).send("No boat with this boat_id exists");
+        } catch (err) {
+            console.log('router.get("/boat_id")');
+            console.log(err);
+            handle_response(res, c.ERROR);
         }
     }
 });
 
 // GET all boats
 router.get('/', async (req, res) => {
+    // check accepts
     const accepts = req.accepts(['application/json']);
     if (!accepts) {
-        res.status(c.NOT_ACCEPTABLE).send("Response must be sent as application/json. Please change Accept request header.");
+        handle_response(res, c.NOT_ACCEPTABLE, 'application/json');
+
     } else {
         try {
+            // get boats from database
             let boats = await get_all_boats();
             boats = boats.map(ds.fromDatastore);
-            res.status(c.OK).json({"boats": boats});
+            handle_response(res, c.OK, [{"boats": boats}, accepts]);
+
         } catch (err) {
             console.log('router.get("/")');
             console.log(err);
+            handle_response(res, c.ERROR);
         }
     }
 })
 
 // BAD ROUTES
 router.delete('/', (req, res) => {
-    res.set('Accept', 'GET, POST');
-    res.status(c.BAD_METHOD).send('Not an acceptable method.');
+    handle_response(res, c.BAD_METHOD);
 });
 
 router.put('/', (req, res) => {
-    res.set('Accept', 'GET, POST');
-    res.status(c.BAD_METHOD).send('Not an acceptable method.');
+    handle_response(res, c.BAD_METHOD);
 });
 
-// router.get('/', (req, res) => {
-//     res.set('Accept', 'PUT');
-//     res.status(c.BAD_METHOD).send('Not an acceptable method.');
-// });
-
 router.patch('/', (req, res) => {
-    res.set('Accept', 'GET, POST');
-    res.status(c.BAD_METHOD).send('Not an acceptable method.');
+    handle_response(res, c.BAD_METHOD);
 })
 
 /* --------------------------------------------------- END CONTROLLER FUNCTIONS ----------------------------------------- */
